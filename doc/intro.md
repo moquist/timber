ce74d279fe13103972aed3b8e128fe1f08f7e52e
Mike George <mikeyg6754@gmail.com>
2014-03-31 16:08:04 -0400

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to timber
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/timber/core_test.clj
@@ -0,0 +1,7 @@
+(ns timber.core-test
+  (:require [clojure.test :refer :all]
+            [timber.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
