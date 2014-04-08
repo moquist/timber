8f63bd9e7ed335a70623b9d4c78af3caeca74fb1
Mike George <mikeyg6754@gmail.com>
2014-04-08 14:23:45 -0400

    add resources

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject timber "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject vlacs/timber "0.1.0-SNAPSHOT"
+  :description "UI Library for VLACS applications"
+  :url "http://vlacs.org"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
