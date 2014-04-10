1150ab896cdaa2ebb142d5a335e0907b65132b06
Jon Doane <jdoane@vlacs.org>
2014-04-10 11:03:47 -0400

    Moved resources over from traveler. Added enlive selectors for template.

--- src/timber/core.clj
+++ src/timber/core.clj
@@ -1,10 +1,16 @@
 (ns timber.core
   (:require [net.cgrand.enlive-html :as html]))
 
-(def bootstrap-css-selector [[:link (html/attr-has :id "bootstrap-css")]])
-(def bootstrap-css-theme-selector [[:link (html/attr-has :id "bootstrap-theme-css")]])
-(def bootstrap-js-selector [[:script (html/attr-has :href "bootstrap.js")]])
+(def title-selector [[:title]])
 
-(def user-name-selector [[:span#username]])
+(def link-asset-selector [[:link (html/attr? :asset)]])
+(def js-asset-selector [[:script (html/attr? :asset)]])
 
+(def main-menu-selector [[:ul#main-menu]])
+(def side-menu-selector [[:ul#side-menu]])
+
+(def link-template-selector [[:link (html/attr? :template)]])
+(def script-template-selector [[:script (html/attr? :template)]])
+
+(def content-selector [[:div#content]])
 
