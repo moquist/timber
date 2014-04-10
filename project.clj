a9df074e2b1b98c67507fc002dd18dc7394d2bdd
Jon Doane <jdoane@vlacs.org>
2014-04-10 12:10:42 -0400

    Moved resource into timber sub-dir. Added enlive to projects.clj Upgraded to Clojure 1.6.0. Started writing generic template call.

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "http://vlacs.org"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :pedantic? :warn
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [enlive "1.1.5"]])
--- src/timber/core.clj
+++ src/timber/core.clj
@@ -14,3 +14,25 @@
 
 (def content-selector [[:div#content]])
 
+(defn default-transformations
+  [request]
+  {content-selector [(html/content "foo")]})
+
+(defn template
+  [request selector-map]
+  (let [merged-map (merge-with #(conj %1 %2) (default-transformations request) selector-map)
+        ;;;transform-map (into {} (map #([(first %1) (apply html/do-> (second %1))]) merged-map))
+        get-selector (partial get merged-map)
+        ]
+    (prn merged-map)
+    (html/template "timber/templates/base.html"
+                   [get-selector]
+                   title-selector (get-selector title-selector)
+                   link-asset-selector (get-selector link-asset-selector)
+                   js-asset-selector (get-selector js-asset-selector)
+                   main-menu-selector (get-selector main-menu-selector)
+                   side-menu-selector (get-selector side-menu-selector)
+                   link-template-selector (get-selector link-template-selector)
+                   script-template-selector (get-selector script-template-selector)
+                   content-selector (get-selector content-selector))))
+
