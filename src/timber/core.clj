46d18f1809fd355d159320fa517131165be90e8b
Mike George <mikeyg6754@gmail.com>
2014-04-30 16:34:25 -0400

    move style and script transforms to bottom

--- src/timber/core.clj
+++ src/timber/core.clj
@@ -23,36 +23,41 @@
                         [:li :a] (html/content menu-name)
                         [:li :a] (html/set-attr :href menu-url)))
 
-(html/defsnippet extra-styles
-  "timber/templates/tags/link.html"
-  [:link]
-  [stylesheet-uris]
-  [:link] (html/clone-for [uri stylesheet-uris]
-                          [:link] (html/set-attr :href uri)))
 
-(html/defsnippet extra-scripts
-  "timber/templates/tags/script.html"
-  [:script]
+(defn extra-styles
+  "Get the first link tag in the template and clone it"
+  [stylesheet-uris]
+  (html/transform
+   (select/first-of-type :link "base.html")
+   [:link] (html/clone-for [uri stylesheet-uris]
+                           [:link] (html/set-attr :href uri))))
+
+
+(defn extra-scripts
+  "Get the first script tag in the template and clone it"
   [script-uris]
-  [:script] (html/clone-for [uri script-uris]
-                            [:script] (html/set-attr :src uri)))
+  (html/transform
+   (select/first-of-type :script "base.html")
+   [:script] (html/clone-for [uri script-uris]
+                             [:script] (html/set-attr :src uri))))
 
 (html/deftemplate
-  base-page
-  "timber/templates/base.html"
-  [{:keys [page-name brand asset-uri-path user-name main-menu
-           user-menu page-content extra-styles extra-scripts
-           ng-app] :as opts}]
+ base-page
+ "timber/templates/base.html"
+ [{:keys [page-name brand asset-uri-path user-name main-menu
+          user-menu page-content extra-styles extra-scripts
+          ng-app] :as opts}]
 
-  select/html            (html/set-attr :ng-app (or ng-app (str "")))
-  select/head            (html/append extra-styles)
-  select/body            (html/append extra-scripts)
+ select/html            (html/set-attr :ng-app (or ng-app (str "")))
 
-  select/title           (html/content page-name)
-  select/brand-title     (html/content (or brand page-name))
-  select/link-asset      (asset/transform-link asset-uri-path)
-  select/js-asset        (asset/transform-script-src asset-uri-path)
-  select/main-menu       (html/content main-menu)
-  select/user-menu       (html/content user-menu)
-  select/user-name       (html/content user-name)
-  select/content         (html/content page-content))
+ select/title           (html/content page-name)
+ select/brand-title     (html/content (or brand page-name))
+ select/link-asset      (asset/transform-link asset-uri-path)
+ select/js-asset        (asset/transform-script-src asset-uri-path)
+ select/main-menu       (html/content main-menu)
+ select/user-menu       (html/content user-menu)
+ select/user-name       (html/content user-name)
+ select/content         (html/content page-content)
+
+ select/head            (html/append extra-styles)
+ select/body            (html/append extra-scripts))
